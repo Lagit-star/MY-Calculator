e11dc872771bbcffbe2cfa7c95898899ce193a99
Lagit <83742377+Lagit-star@users.noreply.github.com>
2021-09-18 23:53:03 +0530

    Add files via upload

---
+++ banner.sh
@@ -0,0 +1,13 @@
+#!/usr/bkn/bash 
+
+
+sleep 5
+
+figlet           ..........MY.......... | lolcat 
+figlet           .. Calculator .. | lolcat 
+
+echo  'WELCOME TO MY DARK WORLD OF LAGIT' | lolcat
+
+sleep 3 
+
+
